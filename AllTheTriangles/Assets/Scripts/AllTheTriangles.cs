0f60dfb64a31c4eb8d3738c2563980d51f671ae8
emedmaria <emedmaria@gmail.com>
2017-12-06 22:17:55 +0100

    Make base abstract class for MeshCreator. Added Normals, tangents, vertexColors

--- AllTheTriangles/Assets/Scripts/AllTheTriangles.cs
+++ AllTheTriangles/Assets/Scripts/AllTheTriangles.cs
@@ -1,82 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// To render the mesh we also need a MeshFilter -is a container which passes the meshes information to the Renderer - and a MeshRender - renders the mesh-
-/// </summary>
-[RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(MeshRenderer))]
-/// <summary>
-/// This will call the Update method whenever something changes in the scene.
-/// </summary>
-[ExecuteInEditMode] 
-public class AllTheTriangles : MonoBehaviour {
 
-    [SerializeField] private Material material;
-    [SerializeField] private Vector3 []  vs = new Vector3[3]; //Going from vertex 0 to 1 to 2 only works if they are positioned, so that the progression is Clockwise. 
-    private List<Vector3> vertices;
-    private List<int> triangles; 
+public class AllTheTriangles : AbstractMeshGenerator {
 
+	protected override void SetMeshNums()
+	{
+		numVertices = 3;
+		numTriangles = 3;
+	}
 
-    /// <summary>
-    /// Stores the mesh information (vertices, triangles, normals, etc. )
-    /// </summary>
-    private Mesh mesh;
-    private MeshFilter meshFilter;
-    private MeshRenderer meshRenderer;
-
-    private void Awake()
-    {
-        meshFilter = this.GetComponent<MeshFilter>();
-        meshRenderer = this.GetComponent<MeshRenderer>();
-
-    }
-
-    private void Update()
-    {
-        //MeshRenderer needs a material to display the mesh
-        meshRenderer.material = material;
-
-        //initialise Vertex and Triangles
-        vertices = new List<Vector3>();
-        triangles = new List<int>();
-
-        //Create the Mesh
-        CreateMesh();
-    }
-
-    private void CreateMesh()
-    {
-        mesh = new Mesh();
-        SetVertices();
-        SetTriangles();
-
-        ///This should aleays be done in this order (Vertices first, Triangles second)
-        mesh.SetVertices(vertices);
-        mesh.SetTriangles(triangles, 0);  // 0 we use a single material
-
-        //Set the meshfilter to be the mesh that just created
-        //meshFilter.mesh = mesh;  //.mesh uses a copy rather .sharedMesh
-        meshFilter.sharedMesh = mesh; 
-
-    }
-
-    private void SetVertices()
+	protected override void SetVertices()
     {
         vertices.AddRange(vs);
     }
 
-    /// <summary>
-    /// The triangles use to join the vertices in a model
-    /// </summary>
-    private void SetTriangles()
+	/// <summary>
+	/// The triangles use to join the vertices in a model
+	/// </summary>
+	protected override void SetTriangles()
     {
-        //This means the mesh starts at Vertex 0, connects to vertex 1, then to vertex 2
-        //The index of the vertices in the triangles list corresponds with the order in which the vertices are joined up
-        //And the integer entry of the index is equal to the vertices index! triangles[0]=0; vertices[0]
-        triangles.Add(0);
-        triangles.Add(1);
-        triangles.Add(2);
-
+		//This means the mesh starts at Vertex 0, connects to vertex 1, then to vertex 2
+		//The index of the vertices in the triangles list corresponds with the order in which the vertices are joined up
+		//And the integer entry of the index is equal to the vertices index! triangles[0]=0; vertices[0]
+		if(!reverseTriangles)
+		{
+			triangles.Add(0);
+			triangles.Add(1);
+			triangles.Add(2);
+		}
+		else
+		{
+			//Changes the order how the Triangles are in (anti-clockwise) - BackFace Culling
+			triangles.Add(0);
+			triangles.Add(2);
+			triangles.Add(1);
+		}
     }
+
+	protected override void SetNormals()
+	{
+	}
+
+	protected override void SetTangents()
+	{
+	}
+
+	protected override void SetUVs()
+	{
+
+	}
+
+	protected override void SetVertecColours()
+	{
+
+	}
 }
